7d70f4f6cfedcfb3c5d5366c51f653247924a4b7
Pablo Yza <p.deyzaguirre13@gmail.com>
2018-12-14 14:44:47 +0100

    Display list of Heroes and show name & details

---
+++ Tour-Of-Heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Magneto'},
+    {id: 12, name: 'Dr.Charles'},
+    {id: 13, name: 'Beast'},
+    {id: 14, name: 'Phoenix'},
+    {id: 15, name: 'Nightcrawler'},
+    {id: 16, name: 'Wolverine'},
+    {id: 17, name: 'Mystique'},
+    {id: 18, name: 'Storm'},
+    {id: 19, name: 'Cyclops'},
+    {id: 20, name: 'Rogue'}
+]
